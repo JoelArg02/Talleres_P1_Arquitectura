04df4dadf80cc35768a2828196bb0245e3a66c6b
Joel Daniel Arguello Espinosa <joel.darguello@gmail.com>
2025-11-11 21:18:38 -0500

    Refactor EurekaBankClient and update JDK platform
    Refactored EurekaBankClient to use simplified imports and type declarations for better readability. Updated project properties and configuration files to use JDK_17 instead of Zulu_17.0.17_10. Cleaned up NetBeans private.xml files by removing open file references.

--- TI1.5 SOAP_JAVA_CONBDD_EUREKABANK_GR03/02. CLICON/eurekabank_consola/src/ec/edu/gr03/model/EurekaBankClient.java
+++ TI1.5 SOAP_JAVA_CONBDD_EUREKABANK_GR03/02. CLICON/eurekabank_consola/src/ec/edu/gr03/model/EurekaBankClient.java
@@ -1,41 +1,46 @@
 package ec.edu.gr03.model;
 
+import ec.edu.gr03.controller.Cuenta;
+import ec.edu.gr03.controller.Movimiento;
+import ec.edu.gr03.controller.WSEureka;
+import ec.edu.gr03.controller.WSEureka_Service;
+import java.util.List;
+
 public class EurekaBankClient {
 
-    public static boolean login(java.lang.String username, java.lang.String password) {
-        ec.edu.gr03.controller.WSEureka_Service service = new ec.edu.gr03.controller.WSEureka_Service();
-        ec.edu.gr03.controller.WSEureka port = service.getWSEurekaPort();
+    public static boolean login(String username, String password) {
+        WSEureka_Service service = new WSEureka_Service();
+    WSEureka port = service.getWSEurekaPort();
         return port.login(username, password);
     }
 
-    public static int regDeposito(java.lang.String cuenta, double importe) {
-        ec.edu.gr03.controller.WSEureka_Service service = new ec.edu.gr03.controller.WSEureka_Service();
-        ec.edu.gr03.controller.WSEureka port = service.getWSEurekaPort();
+    public static int regDeposito(String cuenta, double importe) {
+        WSEureka_Service service = new WSEureka_Service();
+        WSEureka port = service.getWSEurekaPort();
         return port.regDeposito(cuenta, importe);
     }
 
-    public static int regRetiro(java.lang.String cuenta, double importe) {
-        ec.edu.gr03.controller.WSEureka_Service service = new ec.edu.gr03.controller.WSEureka_Service();
-        ec.edu.gr03.controller.WSEureka port = service.getWSEurekaPort();
+    public static int regRetiro(String cuenta, double importe) {
+        WSEureka_Service service = new WSEureka_Service();
+        WSEureka port = service.getWSEurekaPort();
         return port.regRetiro(cuenta, importe);
     }
 
-    public static int regTransferencia(java.lang.String cuentaOrigen, java.lang.String cuentaDestino, double importe) {
-        ec.edu.gr03.controller.WSEureka_Service service = new ec.edu.gr03.controller.WSEureka_Service();
-        ec.edu.gr03.controller.WSEureka port = service.getWSEurekaPort();
+    public static int regTransferencia(String cuentaOrigen, String cuentaDestino, double importe) {
+        WSEureka_Service service = new WSEureka_Service();
+        WSEureka port = service.getWSEurekaPort();
         return port.regTransferencia(cuentaOrigen, cuentaDestino, importe);
     }
 
-    public static java.util.List<ec.edu.gr03.controller.Movimiento> traerMovimientos(java.lang.String cuenta) {
-        ec.edu.gr03.controller.WSEureka_Service service = new ec.edu.gr03.controller.WSEureka_Service();
-        ec.edu.gr03.controller.WSEureka port = service.getWSEurekaPort();
+    public static List<Movimiento> traerMovimientos(String cuenta) {
+        WSEureka_Service service = new WSEureka_Service();
+        WSEureka port = service.getWSEurekaPort();
         return port.traerMovimientos(cuenta);
     }
 
-    public static java.util.List<ec.edu.gr03.controller.Cuenta> traerBalances() {
-        ec.edu.gr03.controller.WSEureka_Service service = new ec.edu.gr03.controller.WSEureka_Service();
-        ec.edu.gr03.controller.WSEureka port = service.getWSEurekaPort();
+    public static List<Cuenta> traerBalances() {
+        WSEureka_Service service = new WSEureka_Service();
+        WSEureka port = service.getWSEurekaPort();
         return port.traerBalances();
     }
-    
 }
